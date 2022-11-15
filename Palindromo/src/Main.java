77b950add30de3414be55a3bbb218cb42a6a572a
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2022-11-15 14:46:17 -0300

    Palíndromos exemplos
    Segunda forma de ver se existem palíndromo.

--- Palindromo/src/Main.java
+++ Palindromo/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
                aux+= value.charAt(i);
             }
         
-            if (!value.equals(aux)){
+            if (!value.equalsIgnoreCase(aux)){
                 return false;
             } else{
                 return true;
@@ -19,9 +19,38 @@ public class Main {
         }
     }
 
+    public static boolean palindrome2(String value){
+        value = value.replaceAll("\\s", "");
+        String aux = "";
+
+        if(!value.isEmpty()){ 
+            for (int i = (value.length()-1); i >= 0; i--){
+               aux+= value.charAt(i);
+            }
+        
+            if (!value.equalsIgnoreCase(aux)){
+                return false;
+            } else{
+                return true;
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean palindrome3(String value){
+        
+        return true;
+
+    }
+
     public static void main(String[] args) {
         
-        String value = "madam";
-        System.out.println(palindrome(value));
+        //String value = "Madam";
+        String value2 ="race car";
+
+        System.out.println("Primeira forma: " + palindrome(value2));
+        System.out.println("Segunda forma: " + palindrome2(value2));
     }
 }
